ab207b5ddd4cdde838e4cca54764530db26aaecd
mck <125995248+MFAS2025@users.noreply.github.com>
2025-07-21 15:08:22 +0800

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,10 +2,10 @@
 setInterval(() => {
   const heart = document.createElement('div');
   heart.classList.add('heart');
-  heart.textContent = '❤️'; // You can randomize with 💖, 💘, 💗 if you want
+  heart.textContent = '❤️';
   heart.style.left = Math.random() * 100 + 'vw';
   heart.style.top = '100vh';
   document.getElementById('floatingHearts').appendChild(heart);
 
-  setTimeout(() => heart.remove(), 6000); // Match animation duration
+  setTimeout(() => heart.remove(), 6000);
 }, 500);
